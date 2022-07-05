574bae2b50682523a08e2a85ca309669fa7d4553
Brooke Chalmers <breq@breq.dev>
2022-07-05 19:11:45 -0400

    mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const USERS = [
 
 function App() {
   return (
-    <article className="bg-rainbow-stripes min-h-screen p-4 md:p-8">
+    <article className="bg-rainbow-stripes min-h-screen p-4 sm:p-8">
       <div className="bg-white border-black border-2 rounded-3xl w-full max-w-xl mx-auto p-8 text-4xl text-center font-bold mb-8">
         poggers.lgbt user directory
       </div>
@@ -35,14 +35,21 @@ function App() {
         {USERS.map((user) => (
           <li
             key={user.email}
-            className="bg-white border-black border-2 rounded-3xl p-8 flex"
+            className="bg-white border-black border-2 rounded-3xl p-8 flex flex-col sm:flex-row text-center sm:text-left gap-2"
           >
-            <span className="w-2/3 text-5xl">{user.name}</span>
-            <div className="w-full flex flex-col text-lg">
-              <a href={`mailto:${user.email}@poggers.lgbt`}>
-                <FontAwesomeIcon icon={faEnvelope} /> {user.email}@poggers.lgbt
+            <span className="sm:w-2/3 text-5xl">{user.name}</span>
+            <div className="sm:w-full flex flex-col text-lg">
+              <a
+                href={`mailto:${user.email}@poggers.lgbt`}
+                className="truncate"
+              >
+                <FontAwesomeIcon icon={faEnvelope} />{" "}
+                <span className="font-bold">{user.email}</span>@poggers.lgbt
               </a>
-              <a href={`https://twitter.com/${user.twitter}`}>
+              <a
+                href={`https://twitter.com/${user.twitter}`}
+                className="truncate"
+              >
                 <FontAwesomeIcon icon={faTwitter} /> @{user.twitter}
               </a>
             </div>
